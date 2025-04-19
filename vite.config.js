0e67ad886c8d7a7c20b89631a507f61ccd46a23a
An Vo <votanlean@gmail.com>
2025-04-19 17:41:22 +0700

    Init

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
